6a73167266a882048d30b3ddd26e3e81d7014f8e
teo3300 <matteo.rogora@live.it>
2023-06-29 15:10:44 +0200

    Binded database to requests
    Binded database to requests, defined baseURL for absolute path access,
    added missing dependencies to project
    
    Signed-off-by: teo3300 <matteo.rogora@live.it>

--- vue-project/nuxt.config.ts
+++ vue-project/nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: [
+        '~/assets/css/general.css'
+    ],
 
+    serverHandlers: [
+        {
+            route: '/server',
+            handler: '~/server/myServer/index.js',
+            middleware: true
+        }
+    ],
+    ssr: true,
+    runtimeConfig: {
+        public: {
+            baseURL: "http://localhost:3000"
+        }
+    }
 })
