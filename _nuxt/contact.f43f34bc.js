21dd3142fb080f8d97a5b57a43de0b4ef912b9bc
teo3300 <matteo.rogora@live.it>
2023-06-29 18:32:24 +0200

    Updates

--- _nuxt/contact.f43f34bc.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,f as _,o as c}from"./entry.a85482dd.js";const t={};function n(o,a){return c(),_("main")}const r=e(t,[["render",n],["__scopeId","data-v-3fbbe8e8"]]);export{r as default};
---
+++ _nuxt/index.cf09de07.js
@@ -0,0 +1,1 @@
+import{_ as n,a as t}from"./CustomFooter.71329038.js";import{_ as c,f as s,b as e,o as r}from"./entry.f4ab424c.js";import"./nuxt-link.2bb763a8.js";const a={};function m(p,u){const o=n,_=t;return r(),s("div",null,[e(o),e(_)])}const l=c(a,[["render",m]]);export{l as default};
