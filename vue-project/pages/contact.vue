29403365a2a6544c425d53b78e8e4ae9c56cf5e9
nirenee <ireonsca@gmail.com>
2023-07-04 02:34:14 +0200

    Update contact.vue

--- vue-project/pages/contact.vue
+++ vue-project/pages/contact.vue
@@ -1,6 +1,23 @@
 <template>
     <main>
+        <CustomHeader></CustomHeader>
+        <h1>GET IN TOUCH</h1>
+        <form>
+            <div>
+                <label for="name" id="name">Name(*)</label>
+                <input type="text" id="iname" required>
+
+                <label for="email" id="email">Email Adress(*)</label>
+                <input type="email" id="iemail" required>
+
+                <label for="compny" id="company">Company</label>
+
+                <label for="name" id="message">The Message</label>
+                <textarea id="message1"></textarea>
+            </div>
         
+        </form>
+        <CustomFooter></CustomFooter>
     </main>
 </template>
 
