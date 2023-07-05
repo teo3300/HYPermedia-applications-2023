fd8771eb9338c7afced4db471141e3e1128e5c55
nirenee <ireonsca@gmail.com>
2023-07-05 20:20:46 +0200

    Update contact.vue

--- vue-project/pages/contact.vue
+++ vue-project/pages/contact.vue
@@ -1,73 +1,36 @@
 <template>
     <main>
         <CustomHeader></CustomHeader>
-        <h1>GET IN TOUCH</h1>
-        <form @submit="submitForm">
-            <div class="errors" v-show="errors.length >0">
-                <li v-for="(error ,index) in errors" :key="index">{{ error }}</li>
-            </div>
-            <div>
-                <label for="name" id="name">Name(*)</label>
-                <input type="text" id="iname" v-model="name" required>
-
-                <label for="email" id="email">Email Adress(*)</label>
-                <input type="email" id="iemail" v-model="email" required>
-
-                <label for="company" id="company">Company</label>
-                <input type="text" id="iname" v-model="company" required>
-
-                <label for="name" id="message">The Message</label>
-                <textarea id="message1"></textarea>
-
-                <button type="submit">Submit</button>
-            </div>
         
-        </form>
+        <h1>GET IN TOUCH</h1>
+        <div class="form">
+        <FormContactus></FormContactus>
+    </div>
         <CustomFooter></CustomFooter>
     </main>
 </template>
 
-<script>
-export default {
-    data() {
-        return{
-            form: {
-                name: '',
-                email:'',
-                company:'',
-                message1:'',
-            },
-            errors:[]
-        };
-    },
-    methods:{
-        submitForm(event){
-            event.preventDefault();
 
-            this.errors =[]
-            if(!this.name ){
-                this.errors.push("name is empty")
-            }
-            if(!this.email){
-                this.errors.push("name is empty")
-            }
-
-
-            if(this.$refs.form.checkValidity()){
-                console.log('Form submitted');
-            }else{
-                console.log('Form validation failed');
-            }
-        },
-    },
-};
-</script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Alice&family=Cambay:wght@400;700&family=Crushed&family=Lato:wght@300;400&display=swap');
     main {
         width: 100%;
         display: flex;
         flex-direction: column;
         align-items: center;
     }
+    h1{
+        font-family: 'Crushed', sans-serif;
+        text-align: left;
+        margin-right: 70%;
+        display: inline-block;
+        max-width: 50%;
+       
+       
+    }
+   
+   
+   
+
 </style>
