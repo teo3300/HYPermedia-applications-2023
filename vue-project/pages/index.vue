5a779769063c5d850cad1116832a81e65526b254
nirenee <ireonsca@gmail.com>
2023-07-06 12:26:23 +0200

    Update index.vue

--- vue-project/pages/index.vue
+++ vue-project/pages/index.vue
@@ -1,7 +1,33 @@
 <template>
   <div>
     <CustomHeader></CustomHeader>
-    
+    <div class="description"></div>
+    <div class="areas">
+      <div id="technology">
+        <img src="https://c.pxhere.com/images/08/8f/d3b845da427417decd18d7e6887c-1448611.jpg!d" href="">
+        <h2>TECHNOLOGY AND SOFTWARE</h2>
+      </div>
+      <div id="healthcare">
+        <img src="" href="">
+        <h2>HEALTHCARE</h2>
+      </div>
+      <div id="energy">
+        <img src="" href="">
+        <h2>Clean Energy & Sustainability</h2>
+      </div>
+      <div id="E-COMERCE">
+        <img src="" href="">
+        <h2>E-commerce & consumer goods</h2>
+      </div>
+    </div>
+
+
+    <div class="people"></div>
+    <img src="" href="">
+    <img src="" href="">
+    <img src="" href="">
+    <img src="" href="">
+    <div class="projects"></div>
     <CustomFooter></CustomFooter>
   </div>
 </template>
