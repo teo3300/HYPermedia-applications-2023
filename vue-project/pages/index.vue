a960867be0429f919d5687b50ec9a660c713d123
nirenee <ireonsca@gmail.com>
2023-07-06 17:31:31 +0200

    Update index.vue

--- vue-project/pages/index.vue
+++ vue-project/pages/index.vue
@@ -3,21 +3,25 @@
     <CustomHeader></CustomHeader>
     <div class="description"></div>
     <div class="areas">
-      <div id="technology">
-        <img src="https://c.pxhere.com/images/08/8f/d3b845da427417decd18d7e6887c-1448611.jpg!d" href="">
+      <div class="areastext">
         <h2>TECHNOLOGY AND SOFTWARE</h2>
+        <img src="../images/technologyAndSoftware.jpg" href="">
+       
       </div>
-      <div id="healthcare">
-        <img src="" href="">
+      <div id="healthcare" class="areastext">
         <h2>HEALTHCARE</h2>
+        <img src="../images/healthcareAndBiotechnology.jpg" href="">
+        
       </div>
-      <div id="energy">
-        <img src="" href="">
+      <div id="energy" class="areastext">
         <h2>Clean Energy & Sustainability</h2>
+        <img src="../images/cleanEnergyAndSustainability.jpg" href="">
+       
       </div>
-      <div id="E-COMERCE">
-        <img src="" href="">
+      <div id="E-COMERCE" class="areastext">
         <h2>E-commerce & consumer goods</h2>
+        <img src="../images/Ecommerce.jpg" href="">
+       
       </div>
     </div>
 
@@ -37,4 +41,45 @@
 
 <style >
 @import url('https://fonts.googleapis.com/css2?family=Cambay:wght@400;700&family=Crushed&family=Lato:wght@300;400&display=swap');
+
+.areas{
+  background-color: #3A506B;
+  display: flex;
+  height:240px;
+  padding-top: 20px;
+  width:fit-content;
+  align-items: flex-end;
+  text-align: center;
+  
+}
+.areas img{
+  display: inline-block;
+  margin-right: 10px;
+  position:relative;
+  height:200px;
+  padding: 20px;
+}
+.areas h2{
+  position:absolute;
+  top: 113px;
+  margin-left: 20px;
+  text-align: center;
+  display: flex;
+  font-family: 'Crushed',sans-serif;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  padding-top: 100px;
+  padding-left: 100px;
+  padding-right: 80px;
+  font-size: 14px;
+  z-index:1;
+}
+
+.people{
+  background-color: #1C2541;
+}
+
+.projects{
+  background-color: #0b132b;
+}
 </style>
