953a2dfdc751ba2bb3eb819201b9f81a6be79707
nirenee <ireonsca@gmail.com>
2023-07-06 12:26:17 +0200

    Update contact.vue

--- vue-project/pages/contact.vue
+++ vue-project/pages/contact.vue
@@ -14,19 +14,19 @@
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Alice&family=Cambay:wght@400;700&family=Crushed&family=Lato:wght@300;400&display=swap');
-    main {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+    
     h1{
         font-family: 'Crushed', sans-serif;
         text-align: left;
-        margin-right: 70%;
+        margin-right: auto;
         display: inline-block;
         max-width: 50%;
        
+       
+    }
+    .form{
+        display: block;
+        text-align:right;
        
     }
    
