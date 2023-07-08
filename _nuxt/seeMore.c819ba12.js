960316e4af0eb0915a872c17df2133124ef514da
teo3300 <matteo.rogora@live.it>
2023-07-08 11:39:19 +0200

    Updates

--- _nuxt/seeMore.c819ba12.js
+++
@@ -1,1 +0,0 @@
-import{_,o,n,l as c,q as s,m as a}from"./entry.a690928d.js";const r={props:["link"]};function p(u,d,e,i,l,x){const t=a;return o(),n(t,{class:"button",to:e.link},{default:c(()=>[s(" See more ... ")]),_:1},8,["to"])}const f=_(r,[["render",p],["__scopeId","data-v-b43c2652"]]);export{f as _};
